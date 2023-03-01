31c8122a56b4c91b04ef502e92cc88ac26a8c5c3
gustavinhoapm <gustavo.agostinho2021@hotmail.com>
2023-03-01 10:00:41 -0300

    Co-authored-by: MatheusNC <MatheusNC@users.noreply.github.com>

---
+++ README.md
@@ -0,0 +1,17 @@
+# HOGWARTS WORDS CHECKLIST
+
+* Organização CSS e HTML [ ]
+
+* fazer animais Light Home_page(sem logar) [ ]
+
+* tirar quadriculado das foto das casas
+
+* Fazer Dark home_page(sem logar) do jeito unico[ ]
+
+* fazer Página de login[ ]
+
+* fazer Página de cadastro[ ]
+
+* Fazer light home_page(logado) cada casa[ ]
+
+
--- js/scripts.js
+++ js/scripts.js
@@ -18,3 +18,88 @@ changeThemeBtn.addEventListener('change', () => {
 
     localStorage.setItem('theme', document.body.classList.contains('dark') ? 'dark' : 'light');
 });
+
+
+
+
+
+
+window.sr = ScrollReveal({ reset: true});
+
+sr.reveal('.intro',{
+duration: 1700
+
+});
+
+
+sr.reveal('.houses',{
+    rotate: {x: 0, y: 80, z:0},
+    duration:1800
+});
+
+
+sr.reveal('.cards',{
+
+    duration:1800
+});
+
+sr.reveal('.caracters',{
+
+    duration:1800
+});
+
+sr.reveal('.titulo_caracters',{
+
+    duration:1800
+});
+
+sr.reveal('.grifinoria_persona_section',{
+    rotate: {x: 2, y: 80, z:2},
+    duration:1800
+});
+
+sr.reveal('.sonserina_persona_section',{
+    rotate: {x: 3, y: 80, z:3},
+    duration:1800
+});
+
+
+sr.reveal('.lufa_lufa_persona_section',{
+    rotate: {x: 3, y: 80, z:3},
+    duration:1800
+});
+
+
+sr.reveal('.corvinal_persona_section',{
+    rotate: {x: 3, y: 80, z:3},
+    duration:1800
+});
+
+sr.reveal('.corvinal_persona_section',{
+    rotate: {x: 3, y: 80, z:3},
+    duration:1800
+});
+
+
+sr.reveal('.creatures',{
+    rotate: {x: 3, y: 80, z:3},
+    duration:1800
+});
+
+sr.reveal('.titulo_creatures',{
+    rotate: {x: 3, y: 80, z:3},
+    duration:1800
+});
+
+sr.reveal('.lista_criaturas',{
+    rotate: {x: 3, y: 80, z:3},
+    duration:1800
+});
+
+
+
+
+
+
+
+
